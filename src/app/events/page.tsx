e24d12b91b96a544c5a4c810b50c39d9e1a4b3a6
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2024-11-15 14:50:31 +0300

    temp fix for events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,69 +1,79 @@
-import CitiesCarousel from '@/components/Custom/Events/CitiesCarousel'
-import FiltredEventsLister from '@/components/Custom/Events/FiltredEventsLister'
-import EventLoadingCard from '@/components/Custom/loaders/EventLoadingCard'
-import { Event } from '@/lib/types'
-import { Suspense } from 'react'
+// import CitiesCarousel from '@/components/Custom/Events/CitiesCarousel'
+// import FiltredEventsLister from '@/components/Custom/Events/FiltredEventsLister'
+// import EventLoadingCard from '@/components/Custom/loaders/EventLoadingCard'
+// import { Event } from '@/lib/types'
+// import { Suspense } from 'react'
 
 
-const FetchEvents = async (): Promise<Response> => {
+// const FetchEvents = async (): Promise<Response> => {
 
-    const eventsApiUrl = `${process.env.NEXT_PUBLIC_URL}/api/events`
+//     const eventsApiUrl = `${process.env.NEXT_PUBLIC_URL}/api/events`
 
-    const res = await fetch(eventsApiUrl, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        next: {
-            revalidate: 6000,
-            tags: ['events'],
-        }
-    })
+//     const res = await fetch(eventsApiUrl, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         next: {
+//             revalidate: 6000,
+//             tags: ['events'],
+//         }
+//     })
 
-    return res
-}
+//     return res
+// }
 
-const EventsPage = async () => {
+// const EventsPage = async () => {
 
-    const res = await FetchEvents()
-    const eventsList: Event[] | [] = await res.json()
+//     const res = await FetchEvents()
+//     const eventsList: Event[] | [] = await res.json()
 
-    if (res.status !== 200) {
-        return (
-            <div className="w-full mx-auto h-fit flex items-center align-middle justify-center flex-col px-4 py-2">
-                <h1 className="w-full text-3xl font-bold text-left text-primary mb-4 "> Featured Events</h1>
-                <p className="text-yellow-600">
-                    Something went wrong
-                </p>
-                <p>Refresh the page</p>
+//     if (res.status !== 200) {
+//         return (
+//             <div className="w-full mx-auto h-fit flex items-center align-middle justify-center flex-col px-4 py-2">
+//                 <h1 className="w-full text-3xl font-bold text-left text-primary mb-4 "> Featured Events</h1>
+//                 <p className="text-yellow-600">
+//                     Something went wrong
+//                 </p>
+//                 <p>Refresh the page</p>
 
-            </div>
-        )
+//             </div>
+//         )
 
-    }
+//     }
 
-    if (res.status === 200 && eventsList.length === 0) {
-        return (
-            <div className="w-full mx-auto h-fit flex items-center align-middle justify-center flex-col px-4 py-2">
-                <h1 className="w-full text-3xl font-bold text-left text-primary mb-4 "> Featured Events</h1>
-                <p className="text-yellow-600">
-                    No events found
-                </p>
-                <p>Refresh the page</p>
-            </div>
-        )
-    }
+//     if (res.status === 200 && eventsList.length === 0) {
+//         return (
+//             <div className="w-full mx-auto h-fit flex items-center align-middle justify-center flex-col px-4 py-2">
+//                 <h1 className="w-full text-3xl font-bold text-left text-primary mb-4 "> Featured Events</h1>
+//                 <p className="text-yellow-600">
+//                     No events found
+//                 </p>
+//                 <p>Refresh the page</p>
+//             </div>
+//         )
+//     }
 
+//     return (
+//         <div className='w-full h-full px-4 py-2 flex flex-col align-middle justify-center'>
+//             <Suspense fallback={<EventLoadingCard />} >
+//                 <FiltredEventsLister events={eventsList} />
+//             </Suspense>
+
+//             <div className="w-full h-fit p-4">
+//                 <CitiesCarousel />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default EventsPage
+
+import React from 'react'
+
+const EventsPage = () => {
     return (
-        <div className='w-full h-full px-4 py-2 flex flex-col align-middle justify-center'>
-            <Suspense fallback={<EventLoadingCard />} >
-                <FiltredEventsLister events={eventsList} />
-            </Suspense>
-
-            <div className="w-full h-fit p-4">
-                <CitiesCarousel />
-            </div>
-        </div>
+        <div>This page is coming soon</div>
     )
 }
 
